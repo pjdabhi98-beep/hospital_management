dde7ed59f320bd95da460bd0e345c10433e9a4c7
mansi mehta <mansimehta05955@gmail.com>
2026-08-23 11:51:59 +0530

    department xml file uploaded

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,10 @@
         appointments, prescriptions and medical history.
     """,
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/department_views.xml',
+    ],
     'installable': True,
     'application': True,
 }
