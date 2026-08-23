c9401f0d7db2184466a2ff0c80cb1399ec6e7770
Pj Dabhi <pjdabhi98@gmail.com>
2026-08-23 09:55:42 +0530

    chore: create hospital module structure

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0',
+    'category': 'Healthcare',
+    'description': """
+        Hospital Management System
+        Manage patients, doctors, appointments,
+        prescriptions and medical history.
+    """,
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
