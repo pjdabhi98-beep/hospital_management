c699f8c8e187ff609ad3fe1fbf1a8dc2df148379
mansi mehta <mansimehta05955@gmail.com>
2026-08-28 11:28:23 +0530

    add billing model and views

--- __manifest__.py
+++ __manifest__.py
@@ -17,6 +17,7 @@
         'views/department_views.xml',
         'views/prescription_views.xml',
         'views/history_views.xml',
+        'views/billing_views.xml',
 
     ],
     'installable': True,
